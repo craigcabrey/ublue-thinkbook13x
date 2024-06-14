5f19252ad90614c7dc4635638f459bf628a4daa4
Craig Cabrey <craigcabrey@gmail.com>
2024-06-14 10:43:07 -0500

    add signing public key

--- build.sh
+++ build.sh
@@ -12,8 +12,20 @@ RELEASE="$(rpm -E %fedora)"
 # List of rpmfusion packages can be found here:
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
-# this installs a package from fedora repos
-rpm-ostree install screen
+cat <<EOF > /etc/yum.repos.d/_copr:copr.fedorainfracloud.org:alternateved:keyd.repo
+[copr:copr.fedorainfracloud.org:alternateved:keyd]
+name=Copr repo for keyd owned by alternateved
+baseurl=https://download.copr.fedorainfracloud.org/results/alternateved/keyd/fedora-\$releasever-\$basearch/
+type=rpm-md
+skip_if_unavailable=True
+gpgcheck=1
+gpgkey=https://download.copr.fedorainfracloud.org/results/alternateved/keyd/pubkey.gpg
+repo_gpgcheck=0
+enabled=1
+enabled_metadata=1
+EOF
+
+rpm-ostree install keyd mosh tmux
 
 # this would install a package from rpmfusion
 # rpm-ostree install vlc
